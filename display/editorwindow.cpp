71446101c183fe71cbf0b8fda4089480ee4076e6
Vincent Raveneau <vincentraveneau@orange.fr>
2014-02-19 22:41:20 +0100

    MAJ du panneau d'affichage. Résolution de l'erreur d'ouverture d'image sur annuler

--- display/editorwindow.cpp
+++ display/editorwindow.cpp
@@ -13,38 +13,56 @@ EditorWindow::EditorWindow(QWidget *parent) :
     QGraphicsScene* presentationScene = new QGraphicsScene(this);
 
     GraphItem* doorItem = new GraphItem(":/resource/porte.png");
-    doorItem->setPos(0,0);
+    doorItem->setPos(0,20);
     presentationScene->addItem(doorItem);
     QGraphicsTextItem* txtPorte = presentationScene->addText("Porte");
     txtPorte->setPos(60,0);
+    txtPorte->scale(2,2);
 
     GraphItem* elevatorItem = new GraphItem(":/resource/ascenseur.png");
-    elevatorItem->setPos(0,60);
+    elevatorItem->setPos(0,80);
     presentationScene->addItem(elevatorItem);
     QGraphicsTextItem* txtElevator = presentationScene->addText("Ascenseur");
     txtElevator->setPos(60,60);
+    txtElevator->scale(2,2);
 
     GraphItem* stairsItem = new GraphItem(":/resource/escalier.png");
-    stairsItem->setPos(0,120);
+    stairsItem->setPos(0,140);
     presentationScene->addItem(stairsItem);
+    QGraphicsTextItem* txtStairs = presentationScene->addText("Escalier");
+    txtStairs->setPos(60,120);
+    txtStairs->scale(2,2);
 
     GraphItem* exitItem = new GraphItem(":/resource/issueSecours.png");
-    exitItem->setPos(0,180);
+    exitItem->setPos(0,200);
     presentationScene->addItem(exitItem);
+    QGraphicsTextItem* txtExit = presentationScene->addText("Issue de secours");
+    txtExit->setPos(60,180);
+    txtExit->scale(2,2);
 
     GraphItem* qrcodeItem = new GraphItem(":/resource/qrcode.png");
-    qrcodeItem->setPos(0,240);
+    qrcodeItem->setPos(0,260);
     presentationScene->addItem(qrcodeItem);
+    QGraphicsTextItem* txtQRCode = presentationScene->addText("QR Code");
+    txtQRCode->setPos(60,240);
+    txtQRCode->scale(2,2);
 
     GraphItem* wcItem = new GraphItem(":/resource/wc.png");
-    wcItem->setPos(0,300);
+    wcItem->setPos(0,320);
     presentationScene->addItem(wcItem);
+    QGraphicsTextItem* txtWC = presentationScene->addText("WC");
+    txtWC->setPos(60,300);
+    txtWC->scale(2,2);
 
     GraphItem* wcHItem = new GraphItem(":/resource/wcH.png");
-    wcHItem->setPos(0,360);
+    wcHItem->setPos(0,380);
     presentationScene->addItem(wcHItem);
+    QGraphicsTextItem* txtWCH = presentationScene->addText("WC pour handicapés");
+    txtWCH->setPos(60,360);
+    txtWCH->scale(2,2);
 
     ui->itemsPresentationArea->setScene(presentationScene);
+    ui->itemsPresentationArea->resize(ui->itemsPresentationArea->scene()->width(),ui->itemsPresentationArea->scene()->height());
 }
 
 EditorWindow::~EditorWindow()
@@ -55,23 +73,25 @@ EditorWindow::~EditorWindow()
 void EditorWindow::on_newElementButton_clicked()
 {
     QString mapFileName = QFileDialog::getOpenFileName(this, "Choisir une image d'arrière plan", "./", "Images (*.png *.jpg *.bmp)");
-    QStringList fileNameParts = mapFileName.split("/");
+    if(!mapFileName.isEmpty()) {
+        QStringList fileNameParts = mapFileName.split("/");
 
-    MapGraphicsView* elementView = new MapGraphicsView();
-    QPixmap pixmap(mapFileName);
-    QGraphicsPixmapItem* background = new QGraphicsPixmapItem(pixmap);
+        MapGraphicsView* elementView = new MapGraphicsView();
+        QPixmap pixmap(mapFileName);
+        QGraphicsPixmapItem* background = new QGraphicsPixmapItem(pixmap);
 
-    QGraphicsScene* elementScene = new QGraphicsScene(0,0,pixmap.width(),pixmap.height());
-    elementView->setScene(elementScene);
-    elementScene->addItem(background);
+        QGraphicsScene* elementScene = new QGraphicsScene(0,0,pixmap.width(),pixmap.height());
+        elementView->setScene(elementScene);
+        elementScene->addItem(background);
 
-    ui->mapAreaStack->addWidget(elementView);
+        ui->mapAreaStack->addWidget(elementView);
 
-    //elementView->fitInView(background);
-    elementScene->setSceneRect(background->boundingRect());
+        //elementView->fitInView(background);
+        //elementScene->setSceneRect(background->boundingRect());
 
-    ui->elementsComboBox->addItem(fileNameParts.back());
-    ui->elementsComboBox->setCurrentIndex(ui->elementsComboBox->count()-1);
+        ui->elementsComboBox->addItem(fileNameParts.back());
+        ui->elementsComboBox->setCurrentIndex(ui->elementsComboBox->count()-1);
+    }
 }
 
 void EditorWindow::on_zoomInButton_clicked()
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include "xml/parser.h"
 #include <QApplication>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
+    //Utilisation de l'utf8 pour afficher correctement les accents
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("utf8"));
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
